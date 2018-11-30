4bd4c85534ab3c4a8ec3ca320de5e19d45f0a062
Jack <Jack_Wang@asus.com>
2018-11-30 16:52:56 +0800

    add folder

--- ex1/src/pages/home/home.ts
+++ ex1/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {UsersPage} from "../users/users";
+import {ShopPage} from "../shop/shop";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onGotoUsers() {
+    this.navCtrl.push(UsersPage);
+  }
+
+  onGotoShop() {
+    this.navCtrl.push(ShopPage);
   }
 
 }
